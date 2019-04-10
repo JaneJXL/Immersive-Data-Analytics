712b7d394226b43ac960057707ae4f781abdb4bd
JaneJXL <48492248+JaneJXL@users.noreply.github.com>
2019-04-10 15:17:50 -0400

    Update simpleelizaapplication.py

--- Week1/simpleelizaapplication.py
+++ Week1/simpleelizaapplication.py
@@ -3,8 +3,10 @@ good = 'false'
 
 while good == 'false':
     if firstg == 'I am feeling great' or firstg == 'I AM FEELING GREAT' or firstg == 'i am feeling great' or firstg =='Q' or firstg =='q':
+        print(firstg)
         print('Have a nice day!')
         good = 'true'
     else:
         firstg = input('Enter your response here or Q to quit:')
+        print(firstg)
         good == 'false'
